b7432899a785c3cf273edf3a62ca466a7f55b968
Ben Baker <bakerb845@gmail.com>
2026-02-13 14:07:36 -0700

    checking in latest cmake

--- src/subscriber.cpp
+++ src/subscriber.cpp
@@ -1,7 +1,7 @@
 #include <grpc/grpc.h>
 #include "uDataPacketService/subscriberOptions.hpp"
 #include "uDataPacketImportAPI/v1/packet.pb.h"
-//#include "uDataPacketServiceAPI/v1/packet.pb.h"
+#include "uDataPacketServiceAPI/v1/packet.pb.h"
 
 namespace
 {
