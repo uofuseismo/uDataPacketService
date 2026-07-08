6fedd4ff5d96643cd72490e433d119363e91e3e7
Ben Baker <bakerb845@gmail.com>
2026-07-08 08:06:48 -0600

    ensuring server running is false on stop

--- src/server.cpp
+++ src/server.cpp
@@ -109,7 +109,7 @@ public:
         // the start() thread is still inside mServer->Wait() and Shutdown()
         // is what unblocks it.  The server is destroyed with this class,
         // after the start() future has been joined.
-        if (mServer && mServerStarted.load())
+        if (mServer && mServerStarted.exchange(false))
         {
             SPDLOG_LOGGER_INFO(mLogger, "Shutting down service");
             const auto shutdownDeadline = mOptions.getShutdownDeadline();
